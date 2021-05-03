74ad286d54b5776608c34666f9ea4e3ce02a1720
Erwin Hartanto <erwinhaa25@gmail.com>
2021-05-03 22:31:16 +0700

    Ubah logika fetch data provinsi

--- dwiky-darmawansyah/src/components/Provinsi.jsx
+++ dwiky-darmawansyah/src/components/Provinsi.jsx
@@ -1,110 +1,138 @@
 import DataCovid from './DataCovid';
 import DataCovidBaru from './DataCovidBaru';
-import { useParams } from "react-router-dom"
+import { useParams } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Provinsi = () => {
-    const [dataCovidProvinsi, setDataCovidProvinsi] = useState(null);
-    const [listOfProvinsi, setListOfProvinsi] = useState(null);
-    let params = useParams();
-    console.log(params);
+  const [dataCovidProvinsi, setDataCovidProvinsi] = useState(null);
+  const [lastDate, setLastDate] = useState(null);
+  let params = useParams();
+  //   console.log(params);
 
-    useEffect(() => {
-        getDataCovidProvinsi()
-    }, [])
-
-    // const listOfProvinsi = dataCovidProvinsi.list_data.map(itemKey => itemKey.key);
-
-    // fungsi konversi ke Capitalize Each Word
-    // function kapital(str) {
-    //     return str.replace(/\w\S*/g,
-    //         function (txt) {
-    //             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    //         });
-    // }
-
-    // fungsi konversi kebab-case ke Title Case
-    function toTitle(string) {
-        return string
-            .split('-')
-            .map(word => {
-                return word.slice(0, 1).toUpperCase() + word.slice(1)
-            })
-            .join(' ');
-    }
-
+  useEffect(() => {
     const getDataCovidProvinsi = async () => {
-        const url = `https://data.covid19.go.id/public/api/prov.json`
+      const url = `https://data.covid19.go.id/public/api/prov.json`;
 
-        try {
-            const response = await axios.get(url)
-            setDataCovidProvinsi(response.data)
-            // console.log(response.data);
-            const nameOfProvinsi = toTitle(params.namaProvinsi).toUpperCase()
-            const dataProvinsi = response.data.list_data.map(itemKey => {
-                if (itemKey.key === nameOfProvinsi) {
-                    return itemKey
-                }
-            })
-            console.log(dataProvinsi);
-            console.log(response.data.list_data.map(itemKey => itemKey.key === toTitle(params.namaProvinsi).toUpperCase()));
-        } catch (err) {
-            console.log(err);
-        }
-    }
+      try {
+        const response = await axios.get(url);
+        setLastDate(response.data.last_date);
 
-    return (
-        <div>
-            <Container>
-                <h2>{`Jumlah Kasus Covid-19 di ${toTitle(params.namaProvinsi)} Saat Ini`}</h2>
-                <h6>Tanggal : {dataCovidProvinsi && dataCovidProvinsi.last_date}</h6>
-                <Row>
-                    <Col>
-                        {dataCovidProvinsi && dataCovidProvinsi.list_data.some(item => item === listOfProvinsi) &&
-                            <div style={{ color: 'DodgerBlue' }}>
-                                <DataCovid dataCovidApp={dataCovidProvinsi.list_data.indexOf(listOfProvinsi).jumlah_kasus}
-                                    keterangan="Positif" />
-                            </div>
-                        }
-                        {dataCovidProvinsi && dataCovidProvinsi.list_data.some(item => item === listOfProvinsi) &&
-                            <div style={{ color: 'green' }}>
-                                <DataCovid dataCovidApp={dataCovidProvinsi.list_data.indexOf(listOfProvinsi).jumlah_sembuh}
-                                    keterangan="Sembuh" />
-                            </div>
-                        }
-                    </Col>
-                    <Col>
-                        {dataCovidProvinsi && dataCovidProvinsi.list_data.some(item => item === listOfProvinsi) &&
-                            <div style={{ color: 'orange' }}>
-                                <DataCovid dataCovidApp={dataCovidProvinsi.list_data.indexOf(listOfProvinsi).jumlah_dirawat}
-                                    keterangan="Dirawat" />
-                            </div>
-                        }
-                        {dataCovidProvinsi &&
-                            <div style={{ color: 'red' }}>
-                                <DataCovid dataCovidApp={dataCovidProvinsi.list_data.indexOf(listOfProvinsi).jumlah_meninggal}
-                                    keterangan="Meninggal" />
-                            </div>
-                        }
-                    </Col>
-                    <Col>
-                        {dataCovidProvinsi && dataCovidProvinsi.list_data.some(item => item === listOfProvinsi) &&
-                            <DataCovidBaru
-                                dataCovidApp={{
-                                    positif: dataCovidProvinsi.list_data.indexOf(listOfProvinsi).jumlah_kasus,
-                                    dirawat: dataCovidProvinsi.list_data.indexOf(listOfProvinsi).jumlah_dirawat,
-                                    sembuh: dataCovidProvinsi.list_data.indexOf(listOfProvinsi).jumlah_sembuh,
-                                    meninggal: dataCovidProvinsi.list_data.indexOf(listOfProvinsi).jumlah_meninggal
-                                }} />
-                        }
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
-}
+        // Untuk nyimpan key nya
+        const nameOfProv = toTitle(params.namaProvinsi).toUpperCase();
 
+        // Ngambil data pake forEach biar langsung set data ke state
+        response.data.list_data.forEach((data) => {
+          if (data.key === nameOfProv) {
+            setDataCovidProvinsi(data);
+          }
+        });
+        // setDataCovidProvinsi(response.data);
+        // console.log(response.data);
+        // const nameOfProvinsi = toTitle(params.namaProvinsi).toUpperCase()
+        // Map bakal return array
+        // const dataProvinsi = response.data.list_data.map(itemKey => {
+        //     if (itemKey.key === nameOfProvinsi) {
+        //         return itemKey
+        //     }
+        // })
+        // console.log(dataProvinsi);
+        // console.log(
+        //   response.data.list_data.map(
+        //     (itemKey) =>
+        //       itemKey.key === toTitle(params.namaProvinsi).toUpperCase()
+        //   )
+        // );
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-export default Provinsi
+    getDataCovidProvinsi();
+  }, [params.namaProvinsi]);
+
+  // const listOfProvinsi = dataCovidProvinsi.list_data.map(itemKey => itemKey.key);
+
+  // fungsi konversi ke Capitalize Each Word
+  // function kapital(str) {
+  //     return str.replace(/\w\S*/g,
+  //         function (txt) {
+  //             return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  //         });
+  // }
+
+  // fungsi konversi kebab-case ke Title Case
+  function toTitle(string) {
+    return string
+      .split('-')
+      .map((word) => {
+        return word.slice(0, 1).toUpperCase() + word.slice(1);
+      })
+      .join(' ');
+  }
+
+  // Some dihilangkan
+  return (
+    <div>
+      <Container>
+        <h2>{`Jumlah Kasus Covid-19 di ${toTitle(
+          params.namaProvinsi
+        )} Saat Ini`}</h2>
+        <h6>Tanggal : {lastDate && lastDate}</h6>
+        <Row>
+          <Col>
+            {dataCovidProvinsi && (
+              <div style={{ color: 'DodgerBlue' }}>
+                <DataCovid
+                  dataCovidApp={dataCovidProvinsi.jumlah_kasus}
+                  keterangan="Positif"
+                />
+              </div>
+            )}
+            {dataCovidProvinsi && (
+              <div style={{ color: 'green' }}>
+                <DataCovid
+                  dataCovidApp={dataCovidProvinsi.jumlah_sembuh}
+                  keterangan="Sembuh"
+                />
+              </div>
+            )}
+          </Col>
+          <Col>
+            {dataCovidProvinsi && (
+              <div style={{ color: 'orange' }}>
+                <DataCovid
+                  dataCovidApp={dataCovidProvinsi.jumlah_dirawat}
+                  keterangan="Dirawat"
+                />
+              </div>
+            )}
+            {dataCovidProvinsi && (
+              <div style={{ color: 'red' }}>
+                <DataCovid
+                  dataCovidApp={dataCovidProvinsi.jumlah_meninggal}
+                  keterangan="Meninggal"
+                />
+              </div>
+            )}
+          </Col>
+          <Col>
+            {dataCovidProvinsi && (
+              <DataCovidBaru
+                dataCovidApp={{
+                  positif: dataCovidProvinsi.jumlah_kasus,
+                  dirawat: dataCovidProvinsi.jumlah_dirawat,
+                  sembuh: dataCovidProvinsi.jumlah_sembuh,
+                  meninggal: dataCovidProvinsi.jumlah_meninggal,
+                }}
+              />
+            )}
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Provinsi;
